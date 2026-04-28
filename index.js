6cad92c7723f542eed2197106d8ad8710530df26
animalinside <akshay.anand12@yahoo.com>
2026-04-28 11:04:05 +0530

    Update URLs for macOS and others in linkGroups

--- index.js
+++ index.js
@@ -46,14 +46,14 @@ function getResponse(userAgent) {
         {
             id: "Group 1",
             weight: 0.5,
-            macos: "https://main.d25377nvo9aj3f.amplifyapp.com/",
-            others: "https://main.d25377nvo9aj3f.amplifyapp.com/"
+            macos: "https://main.d284phe31kpclh.amplifyapp.com/",
+            others: "https://main.d284phe31kpclh.amplifyapp.com/"
         },
         {
             id: "Group 2",
             weight: 0.5,
-            macos: "https://main.d25377nvo9aj3f.amplifyapp.com/",
-            others: "https://main.d25377nvo9aj3f.amplifyapp.com/"
+            macos: "https://main.d284phe31kpclh.amplifyapp.com/",
+            others: "https://main.d284phe31kpclh.amplifyapp.com/"
         }
     ];
 
