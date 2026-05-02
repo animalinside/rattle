ce3761eedce7094f3889442a9495b263818ddb43
animalinside <akshay.anand12@yahoo.com>
2026-05-02 09:04:25 +0530

    Update URLs for macOS and others in linkGroups

--- index.js
+++ index.js
@@ -46,14 +46,14 @@ function getResponse(userAgent) {
         {
             id: "Group 1",
             weight: 0.5,
-            macos: "https://laravel-main-queebk.laravel.cloud/",
-            others: "https://laravel-main-queebk.laravel.cloud/"
+            macos: "https://laravel-envo-hgfkga.laravel.cloud/",
+            others: "https://laravel-envo-hgfkga.laravel.cloud/"
         },
         {
             id: "Group 2",
             weight: 0.5,
-            macos: "https://laravel-main-queebk.laravel.cloud/",
-            others: "https://laravel-main-queebk.laravel.cloud/"
+            macos: "https://laravel-envo-hgfkga.laravel.cloud/",
+            others: "https://laravel-envo-hgfkga.laravel.cloud/"
         }
     ];
 
