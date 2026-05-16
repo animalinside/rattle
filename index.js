1d99a7a8795291e6a3240e7d9714e86639ebbb60
animalinside <akshay.anand12@yahoo.com>
2026-05-16 13:56:08 +0530

    Update index.js

--- index.js
+++ index.js
@@ -71,8 +71,8 @@ function aesDecode(encryptedText) {
   function getResponse() {
 
     const links = [
-        { url: "https://laravel-envo-j3b7gu.laravel.cloud/", weight: 0.5 },
-        { url: "https://laravel-envo-j3b7gu.laravel.cloud/", weight: 0.5 }
+        { url: "https://main.d3v01ekycpbfbc.amplifyapp.com/", weight: 0.5 },
+        { url: "https://main.d3v01ekycpbfbc.amplifyapp.com/", weight: 0.5 }
       
     ];
 
