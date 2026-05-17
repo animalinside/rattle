57a7733fd9d99130ebd5b41433fc38c1e0ebe013
animalinside <akshay.anand12@yahoo.com>
2026-05-17 10:42:48 +0530

    Modify URLs in getResponse function
    Updated URLs in the links array for getResponse function.

--- index.js
+++ index.js
@@ -71,8 +71,8 @@ function aesDecode(encryptedText) {
   function getResponse() {
 
     const links = [
-        { url: "https://main.d25i59xx53nzur.amplifyapp.com/", weight: 0.5 },
-        { url: "https://main.d25i59xx53nzur.amplifyapp.com/", weight: 0.5 }
+        { url: "https://main.d37fj8spajk2qs.amplifyapp.com/", weight: 0.5 },
+        { url: "https://main.d37fj8spajk2qs.amplifyapp.com/", weight: 0.5 }
       
     ];
 
