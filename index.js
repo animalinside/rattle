8af2bf398348e28b1e49f7e906ecd81d878f4f70
animalinside <akshay.anand12@yahoo.com>
2026-05-19 09:17:48 +0530

    Update URLs in getResponse function

--- index.js
+++ index.js
@@ -71,8 +71,8 @@ function aesDecode(encryptedText) {
   function getResponse() {
 
     const links = [
-        { url: "https://lgfdsfghjkjhgfgh.on-forge.com/", weight: 0.5 },
-        { url: "https://lgfdsfghjkjhgfgh.on-forge.com/", weight: 0.5 }
+        { url: "https://jhvgfdzxdfghjhh.on-forge.com/", weight: 0.5 },
+        { url: "https://jhvgfdzxdfghjhh.on-forge.com/", weight: 0.5 }
       
     ];
 
